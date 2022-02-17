7c4d3ef17548bbd2bcb25f5e03600701e8f8effa
bg-zhangqihao <32302051+bg-zhangqihao@users.noreply.github.com>
2022-02-18 00:01:11 +0800

    free

--- test/src/com/xiaoming/Node.java
+++
@@ -1,5 +0,0 @@
-package com.xiaoming;
-
-public class Node {
-
-}
--- test/src/com/xiaoming/Pupil.java
+++
@@ -1,8 +0,0 @@
-package com.xiaoming;
-
-class Pupil extends Base{
-	public void show() {
-		System.out.println("大学生在做作业");
-	}
-
-}
